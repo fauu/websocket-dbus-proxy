aadc2afd91ab2bf2b274dde214dad482feeb583e
Piotr Grabowski <fau999@gmail.com>
2020-05-18 16:27:58 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,46 @@
+# websocket-dbus-proxy
+
+Creates a websocket server that forwards messages received by the script through dbus. Requires Python 3.
+
+
+## Example use-cases
+
+The script was created to make possible controlling the [LiveSplit One](https://github.com/LiveSplit/LiveSplitOne) speedrunning timer, which is a web application, using global keyboard shortcuts.
+
+## Usage example
+
+The following usage example is based on the particular use-case described above.
+
+First, clone the repository or download and unpack the source. Enter the directory.
+
+Install dependencies, for example:
+
+````bash
+pip install -r requirements.txt
+````
+
+Create a global keyboard shortcut that will send a dbus message to the script, containing a command recognizable by LiveSplit One. The following example binds the `space` key to the LiveSplit One command `splitorstart` in the i3 window manager configuration file:
+
+````
+bindsym space exec dbus-send --session --type=signal / com.github.fauu.websocket_dbus_proxy string:'splitorstart'
+````
+
+Run the script, specifying a port for the websocket server, for example:
+
+````bash
+python websocket-dbus-proxy.py 1111
+````
+
+The script should report the address of the websocket server, for example:
+
+````bash
+Websocket dbus proxy running at ws://localhost:1111…
+````
+
+Open https://one.livesplit.org/.
+
+Click "Connect to Server" button.
+
+Enter the fully qualified websocket server address, in our example `ws://localhost:1111`.
+
+You should now be able to start the timer or split it if it's already running using the `space` button, even with the browser window running LiveSplit One out of focus.
---
+++ websocket-dbus-proxy.py
@@ -0,0 +1,73 @@
+import asyncio
+import socket
+import sys
+
+import dbussy as dbus
+from dbussy import DBUS
+import websockets
+
+
+cmd_queue = asyncio.Queue()
+
+
+async def producer_handler(websocket, path):
+    while True:
+        cmd = await cmd_queue.get()
+        cmd_queue.task_done()
+        print("Forwarded command: '{}'".format(cmd))
+        await websocket.send(cmd)
+
+
+async def message_filter(connection, message, data):
+    if (
+        message.interface == "com.github.fauu"
+        and message.member == "websocket_dbus_proxy"
+    ):
+        cmd = list(message.objects)[0]
+        print("Received command: '{}'".format(cmd))
+        cmd_queue.put_nowait(cmd)
+    return DBUS.HANDLER_RESULT_HANDLED
+
+
+def main(argv):
+    if len(sys.argv) != 2:
+        print(f"Usage: {sys.argv[0]} <port>", file=sys.stderr)
+        return 1
+    port = sys.argv[1]
+
+    start_server = websockets.serve(producer_handler, "localhost", port)
+
+    loop = asyncio.get_event_loop()
+
+    try:
+        loop.run_until_complete(start_server)
+    except socket.gaierror:
+        print(f"Specified port '{port}' is incorrect", file=sys.stderr)
+        return 1
+    except OSError as err:
+        if err.errno == 98:
+            print(
+                f"Specified port '{port}' is already in use (is another instance already running?)",
+                file=sys.stderr,
+            )
+        else:
+            print(f"Unknown operating system error", file=sys.stderr)
+        return 1
+
+    print(f"Websocket dbus proxy running at ws://localhost:{port}…")
+
+    conn = dbus.Connection.bus_get(DBUS.BUS_SESSION, private=False)
+    conn.attach_asyncio(loop)
+    conn.add_filter(message_filter, None)
+    conn.bus_add_match("type=signal")
+
+    print("Waiting for commands…")
+
+    try:
+        loop.run_forever()
+    except KeyboardInterrupt:
+        print("\nExiting…")
+
+
+if __name__ == "__main__":
+    sys.exit(main(sys.argv))
